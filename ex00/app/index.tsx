23ee28548175883bd38fa93c52b1aae351011789
Eric Holzer <eric.holzer.lsn@gmail.com>
2026-02-10 14:22:19 +0900

    Add ex00

---
+++ ex00/app/index.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@react-navigation/elements";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+		gap: 10,
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>hello, you.</Text>
+	  <Button onPressIn={() => { console.log('Button pressed')}}>click me if you dare</Button>
+    </View>
+  );
+}
